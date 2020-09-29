9f0957e6dea525cd8ec905606be517bee91274d7
parsasi <parsasi@rocketmail.com>
2020-09-30 02:24:36 +0330

    SCHEMA.sql file created Database connection is created using TypeOrm. Visit app.module.ts for config CLEAN UP NEEDED: put TypeOrm config in a seperate class and address it using useClass property

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,33 @@
-import { Module , MiddlewareConsumer } from '@nestjs/common';
+import { Module , MiddlewareConsumer  } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FrontendModule } from './frontend/frontend.module';
 import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { FrontendMiddleware } from './common/middlewares/frontendMiddleware'
 import { FrontendService } from './frontend/frontend.service'
 import { FrontendController } from './frontend/frontend.controller'
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [FrontendModule , ConfigModule.forRoot({isGlobal: true,})],
+  imports: [
+    FrontendModule,
+    ConfigModule.forRoot({isGlobal: true,}),
+    TypeOrmModule.forRootAsync({
+      imports : [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('mysql_host'),
+        port: configService.get('mysql_port'),
+        username: configService.get('mysql_username'),
+        password: configService.get('mysql_password'),
+        database: configService.get('mysql_dbname'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    })],
   controllers: [AppController , FrontendController],
   providers: [AppService , FrontendService],
 })
