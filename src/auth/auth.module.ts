26459d618b34b81e52c069092c718d05bf0827ef
parsasi <parsasi@rocketmail.com>
2020-10-05 01:26:35 +0330

    JWT setup login route now tokenizes the user object before sending it to the client /src/auth/constant added to hold the jwtSecret

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module  } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module'
 import { BcryptModule } from '../bcrypt/bcrypt.module'
 import { LocalStrategy } from './local.strategy'
 import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { jwtConstants } from './constant'
+
+
 
 @Module({
-  imports : [UserModule , BcryptModule , PassportModule],
-  providers: [AuthService , LocalStrategy]
+  imports : [UserModule
+            , BcryptModule 
+            , PassportModule 
+            , JwtModule.register({
+                secret : jwtConstants.secret,
+                signOptions: { expiresIn: '7d' },
+            })
+          ],
+  providers: [AuthService , LocalStrategy],
+  exports : [AuthService]
 })
 export class AuthModule {}
