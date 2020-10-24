f4d2861b4714e0f03c6c76fc5597f032e4675c9d
parsasi <parsasi@rocketmail.com>
2020-10-24 03:40:24 +0330

    Working on creating a new session once the request is accepted

--- src/request/request.controller.ts
+++ src/request/request.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Post, UseGuards, HttpStatus , Body , Request , Put } from '@nestjs/common';
+import { Controller, Post, UseGuards, HttpStatus , Body , Request , Put , Response } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard'
 import { CreateRequestDto } from './dto/createRequestDto'
 import { RequestHelperService } from './helpers/request.helper.service'
 import { RequestService } from './request.service'
 import { ResponseRequestDto } from './dto/responseRequestDto'
+import { SessionService } from '../session/session.service'
 
 @Controller('request')
 export class RequestController {
     constructor(
         private requestHelperService : RequestHelperService,
-        private requestService : RequestService 
+        private requestService : RequestService,
+        private sessionService : SessionService
     ){}
 
     @UseGuards(JwtAuthGuard)
@@ -28,8 +30,20 @@ export class RequestController {
 
     @UseGuards(JwtAuthGuard)
     @Put('/respond')
-    async respondRequest(@Body() responseRequestDto : ResponseRequestDto, @Request() req){
-        return await this.requestHelperService.respondRequest(responseRequestDto.isAccepted , responseRequestDto.id , req.user.username)
+    async respondRequest(@Body() responseRequestDto : ResponseRequestDto, @Request() req , @Response() res){
+        const requestRespondResult =  await this.requestHelperService.respondRequest(responseRequestDto.isAccepted , responseRequestDto.id , req.user.username)
+        if(responseRequestDto.isAccepted){
+            const request = await requestRespondResult && await this.requestService.getRequest({id : responseRequestDto.id})
+            const session = {
+                startTime : request.startTime,
+                duration : request.duration,
+                
+            }
+            // this.sessionService.createSession()
+
+        }else{
+            return await requestRespondResult && res.send(HttpStatus.OK)
+        }
     }
 
 }
--- src/request/request.service.ts
+++ src/request/request.service.ts
@@ -6,6 +6,19 @@ import { User } from '../user/user.entity'
 import { Tutor } from '../tutor/tutor.entity'
 import { Category } from '../category/category.entity'
 
+interface respondRequestServiceDto{
+    tutor : Tutor
+    id : number
+}
+
+interface createRequestServiceDto{
+    tutor : Tutor,
+    category : Category,
+    user : User,
+    startTime : Date,
+    duration : number
+}
+
 @Injectable()
 export class RequestService {
     constructor(
@@ -13,7 +26,7 @@ export class RequestService {
         private requestRepository : Repository<Request>,
     ){}
 
-    async createRequest(request : {tutor : Tutor , category : Category , user : User , startTime : Date , duration : number}) : Promise<InsertResult> {
+    async createRequest(request : createRequestServiceDto) : Promise<InsertResult> {
         return await this.requestRepository.insert(request)
     }
 
@@ -21,7 +34,7 @@ export class RequestService {
         return await this.requestRepository.findOne(condition)
     }
 
-    async respondRequest(condition : {tutor : Tutor , id : number} , isAccepted : boolean) : Promise<Request> {
+    async respondRequest(condition : respondRequestServiceDto , isAccepted : boolean) : Promise<Request> {
         
         //Looking for a request with the given ID and with the tutor who is logged in
         //This is to make sure that tutors can only respond to their own requests        
@@ -30,7 +43,7 @@ export class RequestService {
         if(!request){
             throw new HttpException("Unauthorized", HttpStatus.UNAUTHORIZED);
         }
-        if(request.isClosed){
+        else if(request.isClosed){
             throw new HttpException("Request already closed", HttpStatus.METHOD_NOT_ALLOWED);
         }
 
@@ -38,12 +51,5 @@ export class RequestService {
 
         return await this.requestRepository.save(request);
 
-
-
-
     }
-    // const userToUpdate  = await this.userRepository.findOne({id})
-    //     user = {...userToUpdate , ...user}
-    //     return await this.userRepository.save(user)
-
 }
