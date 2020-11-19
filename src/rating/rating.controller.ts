dfab3581e7c5344fe2360408973c4138ce1c8ea7
parsasi <parsasi@rocketmail.com>
2020-11-19 06:28:17 +0330

    DELETE /rating endpoint created but has secutiry issues;

--- src/rating/rating.controller.ts
+++ src/rating/rating.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post , UseGuards , Body , Request } from '@nestjs/common'
+import { Controller, Post , UseGuards , Body , Request , Delete } from '@nestjs/common'
 import { RatingService } from './rating.service'
 import { JwtAuthGuard } from '../auth/jwt-auth.guard'
 import { CreateRatingDto } from './dto/createRatingDto'
+import { DeleteRatingDto } from './dto/deleteRatingDto'
 
 @Controller('rating')
 export class RatingController {
@@ -13,5 +14,12 @@ export class RatingController {
     @Post('/')
     async createRating(@Body() createRatingDto : CreateRatingDto , @Request() req){
         return await this.ratingService.createRating(createRatingDto , req.user.username)
+    }
+
+
+    @UseGuards(JwtAuthGuard)
+    @Delete('/')
+    async deleteRating(@Body() deleteRatingDto : DeleteRatingDto , @Request() req){
+        return await this.ratingService.deleteRating(deleteRatingDto , req.user.username)
     }
 }
